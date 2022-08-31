d73e769bc39652614820140cf732dbf56668da21
Lautaro Bachmann <laurigbach@gmail.com>
2022-08-31 18:26:59 -0300

    Started testing time entries

---
+++ tests/test_posk.py
@@ -0,0 +1,8 @@
+from posk import PoskContext
+import tests.mocks as mocks
+
+
+def test_timer_entries():
+    posk = PoskContext(mocks.MockConfig(), None, None)
+    tasks = mocks.get_mock_tasks()
+    assert posk.get_timer_entries(tasks) == mocks.get_mock_timer_entries()
