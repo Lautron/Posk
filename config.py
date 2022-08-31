6116a3aae983e32405749857bd4c3c87f5b143cc
Lautaro Bachmann <laurigbach@gmail.com>
2022-08-31 12:22:32 -0300

    Added config module

---
+++ config.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class Config:
+    notify_command: str
+    tmp_files_folder: str
+
+
+class ConfigTemplate:
+    @abstractmethod
+    def get_config_path(self):
+        pass
+
+    def get_config(self) -> Config:
+        """Get config data from config folder
+        :returns: dict
+
+        """
+        path = self.get_config_path()
+        pass
+
+
+class WindowsConfig(ConfigTemplate):
+    def get_config_path(self):
+        pass
+
+
+class UnixConfig(ConfigTemplate):
+    def get_config_path(self):
+        pass
