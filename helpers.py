e48df98b36e391a03084403df8c08467e1495ca2
Lautaro Bachmann <laurigbach@gmail.com>
2022-08-31 17:47:43 -0300

    Turned namedtuple into dataclass to facilitate type conversion of member properties

--- helpers.py
+++ helpers.py
@@ -1,3 +1,18 @@
 from collections import namedtuple
+from dataclasses import dataclass
 
-Task = namedtuple("Task", ["description", "project", "work_sets", "break_type"])
+
+@dataclass
+class Task:
+    def __init__(
+        self, description, project, work_sets, take_break_after_how_many_work_sets
+    ):
+        self.description = description
+        self.project = project
+        self.work_sets = int(work_sets)
+        self.take_break_after_how_many_work_sets = int(
+            take_break_after_how_many_work_sets
+        )
+
+
+TimerEntry = namedtuple("TimerEntry", ["type", "description", "duration"])
