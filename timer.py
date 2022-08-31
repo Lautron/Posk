318785e9a1588633e45ce880c9a408452b3ea83f
Lautaro Bachmann <laurigbach@gmail.com>
2022-08-31 18:00:34 -0300

    Created module for timer strategy

---
+++ timer.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from helpers import TimerEntry
+import subprocess
+
+
+class AbstractTimerStrategy(ABC):
+    template = ""
+
+    @abstractmethod
+    def _get_args_from_entry(self, entry: TimerEntry):
+        pass
+
+    @abstractmethod
+    def run(self, entry: TimerEntry) -> None:
+        pass
+
+
+class TermdownStrategy(AbstractTimerStrategy):
+    def _get_args_from_entry(self, entry: TimerEntry):
+        return f"--title {entry.description} {entry.duration}"
+
+    def run(self, entry: TimerEntry) -> None:
+        args = self._get_args_from_entry(entry)
+        subprocess.Popen("termdown" + args)
