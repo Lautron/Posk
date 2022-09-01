75295eea9b492c90f19b3d993fab9130eafe7dda
Lautaro Bachmann <laurigbach@gmail.com>
2022-09-01 12:40:17 -0300

    Marked tests and added integration test

--- tests/manual/test_posk.py
+++ tests/manual/test_posk.py
@@ -9,6 +9,7 @@ def assert_input():
     assert it_worked
 
 
+@pytest.mark.editor
 def test_opening_with_editor():
     posk = mocks.get_mock_posk()
     posk._open_tasks_input_with_editor()
@@ -21,3 +22,12 @@ def test_termdown():
     entries = mocks.get_mock_timer_entries()
     posk.run_timer(entries)
     assert_input()
+
+
+@pytest.mark.integration
+def test_integration():
+    mock_file = mocks.get_mock_csv_file()
+    posk = mocks.get_mock_posk()
+    tasks = posk._parse_tasks(mock_file)
+    entries = posk.get_timer_entries(tasks)
+    posk.run_timer(entries)
