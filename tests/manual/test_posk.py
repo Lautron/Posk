5d21654181d85421ebd9ef1c02ef579c48674f50
Lautaro Bachmann <laurigbach@gmail.com>
2022-09-01 10:08:46 -0300

    Added test for opening editor

--- tests/manual/test_posk.py
+++ tests/manual/test_posk.py
@@ -1,14 +1,23 @@
+# To run add -s flag to pytest
+
 import tests.mocks as mocks
 import pytest
 
-pytestmark = pytest.mark.skip(reason="These tests are executed manually")
+
+def assert_input():
+    it_worked = bool(input("\nWrite anything to pass test:\n"))
+    assert it_worked
 
 
+def test_opening_with_editor():
+    posk = mocks.get_mock_posk()
+    posk._open_tasks_input_with_editor()
+    assert_input()
+
+
+@pytest.mark.skip(reason="Takes too long")
 def test_termdown():
     posk = mocks.get_mock_posk()
     entries = mocks.get_mock_timer_entries()
     posk.run_timer(entries)
-
-
-if __name__ == "__main__":
-    test_termdown()
+    assert_input()
